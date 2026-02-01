f1b27fb355214b9f08dab8c8e4536ac4e29c473c
AashayDoshi <doshiaashay@gmail.com>
2026-02-01 19:07:13 +0530

    Update route.js

--- app/api/ask/route.js
+++ app/api/ask/route.js
@@ -18,8 +18,7 @@ User question: ${question}`;
     console.log('Sending to Gemini API...');
     
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/vv1betam/odels/gemini-1.5-flash-latest:generateContent?key=" +
-      process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +      process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: {
